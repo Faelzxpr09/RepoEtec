5b9697a43348f7cd38cc68510be203843ad1c74a
Faelzxpr09 <rafaelpracidellisilva@gmail.com>
2025-09-19 10:46:44 -0300

    criado atributos, construtor, getters e setters na ClasseDiscente. Modificado atributo na ClassePessoas(enderecoAt)

--- src/ClasseDiscente.java
+++ src/ClasseDiscente.java
@@ -1,4 +1,45 @@
 
-public class ClasseDiscente {
-
+public class ClasseDiscente extends ClassePessoas{
+	
+	int nota1At;
+	int nota2At;
+	int nota3At;
+	int nota4At;
+		
+	
+	public ClasseDiscente(int nota1Par, int nota2Par, int nota3Par, int nota4Par,
+	String NomePar, int idadePar, ClasseEndereco enderecoPar) {
+		super(NomePar, idadePar, enderecoPar);
+		
+		this.nota1At = nota1Par;
+		this.nota2At = nota2Par;
+		this.nota3At = nota3Par;
+		this.nota4At = nota4Par;
+	}
+	
+	public int alterar_nota1() {
+		return nota1At;}
+	
+	public int alterar_nota2() {
+		return nota2At;}
+	
+	public int alterar_nota3() {
+		return nota3At;}
+	
+	public int alterar_nota4() {
+		return nota4At;}
+	
+	
+	public void pegarNota1(int nota) {
+		this.nota1At = nota;}
+	
+	public void pegarNota2(int nota) {
+		this.nota2At = nota;}
+	
+	public void pegarNota3(int nota) {
+		this.nota3At = nota;}
+	
+	public void pegarNota4(int nota) {
+		this.nota4At = nota;}
 }
+
--- src/ClassePessoas.java
+++ src/ClassePessoas.java
@@ -3,15 +3,16 @@ public class ClassePessoas {
 
 	String NomeAt;
 	int idadeAt;
-	ClasseEndereco ClasseEnderecoAt;
+	ClasseEndereco enderecoAt;
 	
 	public ClassePessoas() {
 		
 	}
 	
-	public ClassePessoas(String nomePar, int idadePar) {
+	public ClassePessoas(String nomePar, int idadePar, ClasseEndereco enderecoPar) {
 		this.NomeAt=nomePar;
 		this.idadeAt=idadePar;
+		this.enderecoAt=enderecoPar;
 	}
 	
 	public String getNome() {
@@ -19,10 +20,16 @@ public class ClassePessoas {
 	
 	public int getIdade() {
 		return idadeAt;}
+	
+	public ClasseEndereco getEndereco() {
+		return enderecoAt;}
 
 	public void alterar_nome(String nomePar) {
 		this.NomeAt = nomePar.toUpperCase();}
 	
 	public void alterar_idade(int idadePar) {
 		this.idadeAt = idadePar;}
+	
+	public void alterar_endereco(ClasseEndereco enderecoPar) {
+		this.enderecoAt = enderecoPar;}
 }
