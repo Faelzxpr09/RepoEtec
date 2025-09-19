939d04be6bb20040575d687dc28d588420127fcf
Faelzxpr09 <rafaelpracidellisilva@gmail.com>
2025-09-19 10:03:48 -0300

    criado getters e setters na classe endereço

--- src/ClasseEndereco.java
+++ src/ClasseEndereco.java
@@ -5,4 +5,28 @@ public class ClasseEndereco {
 	String cidadeAt;
 	String estadoAt;
 
+	
+	public ClasseEndereco(String ruaPar, String cidadePar, String estadoPar) {
+		this.ruaAt = ruaPar;
+		this.cidadeAt = cidadePar;
+		this.estadoAt = estadoPar;
+	}
+	
+	public String pegarRua() {
+		return ruaAt;}
+	
+	public String pegarCidade() {
+		return cidadeAt;}
+	
+	public String pegarEstado() {
+		return estadoAt;}
+	
+	public void alterar_rua(String ruaPar) {
+		this.ruaAt = ruaPar;}
+	
+	public void alterar_cidade(String cidadePar) {
+		this.cidadeAt = cidadePar;}
+	
+	public void alterar_estado(String estadoPar) {
+		this.estadoAt = estadoPar;}
 }
