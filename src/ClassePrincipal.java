e6af607596a952d0385ab7925e04fc8e89faaa8d
rafaelpracidellisilva@gmail.com <Faelzxpr09>
2025-09-26 10:10:43 -0300

    finalizado as classes e criado objeto

--- src/ClassePrincipal.java
+++ src/ClassePrincipal.java
@@ -5,6 +5,16 @@ public class ClassePrincipal {
 	public static void main(String[] args) {
 		ClasseEndereco endereco1 = new ClasseEndereco("Laurindo Scavassa", "Assis", "SP");
 		ClasseEndereco endereco2 = new ClasseEndereco();
+		
+		System.out.println(endereco1.pegarRua());
+		System.out.println(endereco1.pegarCidade());
+		System.out.println(endereco1.pegarEstado());
+		
+		endereco2.alterar_cidade("Guarulhos");
+		endereco2.alterar_rua("rua luiz");
+		
+		System.out.println(endereco2.pegarRua());
+		System.out.println(endereco2.pegarCidade());
 	}
 
 }
